ebf715dc031ee078e5e48719bad81ba1c96d1ab6
arenv07 <arenvillano@gmail.com>
2024-03-12 16:45:43 -0400

    Initializing project structure

---
+++ shop/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
